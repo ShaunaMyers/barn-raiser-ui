60200c416e7643f11078666b7e2bf32e51d4441e
Shauna Myers <shaunamyers26@gmail.com>
2021-08-27 21:07:57 +0100

    Comment out form to display needs messages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,11 +9,11 @@ function App() {
         <h1>BarnRaiser</h1>
       </header>
       <section className="help-questions">
-        {/* <h2>Are You Looking for Assistance?</h2>
+        <h2>Are You Looking for Assistance?</h2>
         <button>I Need Help</button>
         <h3>Can You Offer Assistance?</h3>
-        <button>Give Help</button> */}
-        <NeedForm />
+        <button>Give Help</button>
+        {/* <NeedForm /> */}
       </section>
     </main>
   );
