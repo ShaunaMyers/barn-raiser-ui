dc9945c25cbc62a28a03ae58fa6ac89a697dde73
Shauna Myers <shaunamyers26@gmail.com>
2021-08-27 14:05:20 +0100

    Add mock data file

---
+++ src/needsData.js
@@ -0,0 +1,22 @@
+export const needsData = { "allActiveNeeds": 
+  [
+    { "point_of_contact": "MargaretMullaney@comcast.net",
+      "description": "I would like to have my yard raked and my gutters cleaned",
+      "start_time": 1630109520000,
+      "end_time": 1630108920000,
+      "zip_code": 80214,
+      "requested_supporters": 2 },
+     { "point_of_contact": "GroverCleveland@turing.edu",
+      "description": "Organizing a charity event to raise money for a family whose house burned down. Need volunteers to set up tables, serve food, run game booths.",
+      "start_time": 1631664240000,
+      "end_time ": 1631664420000,
+      "zip_code": 80995,
+      "requested_supporters": 5 },
+     { "point_of_contact": "GeorgeElliot@gmail.com",
+      "description": "Will someone come read to me. I prefer the classics.",
+      "start_time": 1631578200000,
+      "end_time ": 1631578320000,
+      "zip_code": 80014,
+      "requested_supporters": 1 }
+  ]
+};
