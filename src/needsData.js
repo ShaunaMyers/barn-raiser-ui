7de21a5281f5318cfeb3fafe52b26d85e6562fe0
Shauna Myers <shaunamyers26@gmail.com>
2021-08-27 21:03:29 +0100

    Add title properties

--- src/needsData.js
+++ src/needsData.js
@@ -1,21 +1,27 @@
 export const needsData = { "allActiveNeeds": 
   [
-    { "point_of_contact": "MargaretMullaney@comcast.net",
+    { 
+      "point_of_contact": "MargeInTheFlesh@comcast.net",
+      "title": "Yard Work",
       "description": "I would like to have my yard raked and my gutters cleaned",
-      "start_time": 1630109520000,
-      "end_time": 1630108920000,
+      "start_time": "10/28/21/12/00/00",
+      "end_time": "10/28/21/16/00/00",
       "zip_code": 80214,
       "requested_supporters": 2 },
-     { "point_of_contact": "GroverCleveland@turing.edu",
+     { 
+      "point_of_contact": "GroverCleveland1234@turing.edu",
+      "title": "Event Volunteers",
       "description": "Organizing a charity event to raise money for a family whose house burned down. Need volunteers to set up tables, serve food, run game booths.",
-      "start_time": 1631664240000,
-      "end_time ": 1631664420000,
+      "start_time": "09/15/21/16/00/00",
+      "end_time ": "09/15/21/19/00/00",
       "zip_code": 80995,
       "requested_supporters": 5 },
-     { "point_of_contact": "GeorgeElliot@gmail.com",
-      "description": "Will someone come read to me. I prefer the classics.",
-      "start_time": 1631578200000,
-      "end_time ": 1631578320000,
+     { 
+      "point_of_contact": "GeorgeElliot4Evuh@gmail.com",
+      "title": "Please Read To Me",
+      "description": "Will someone come read to me? I have lost my eyesight. I prefer the classics.",
+      "start_time": "09/22/21/10/00/00",
+      "end_time ": "09/22/21/12/00/00",
       "zip_code": 80014,
       "requested_supporters": 1 }
   ]
