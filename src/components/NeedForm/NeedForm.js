040cc51a76d1842466f681794358c6e8d84d5924
Shauna Myers <shaunamyers26@gmail.com>
2021-08-27 13:34:41 +0100

    Set up form with inputs and button

--- src/components/NeedForm/NeedForm.js
+++ src/components/NeedForm/NeedForm.js
@@ -1,1 +1,19 @@
 import './NeedForm.css';
+
+const NeedForm = () => {
+    return ( 
+        <form>
+            <input type="text" name="name" placeholder="Name"/>
+            <input type="url" name="email" placeholder="Email Address"/>
+            <input type="text" name="zip-code" placeholder="Zip Code"/>
+            <input type="date" name="need-date" min="2021-08-27" max="2025-08-27"/>
+            <input type="time" name="start-time"/>
+            <input type="time" name="end-time"/>
+            <input type="number" name="volunteers-needed" min="1" max="100"/>
+            <input type="text" name="need-description" placeholder="Describe your need"/>
+            <button>Submit</button>
+        </form>
+     );
+}
+ 
+export default NeedForm;
