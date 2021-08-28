35cd923db12680b74053f2c62533e364d1fc800b
Shauna Myers <shaunamyers26@gmail.com>
2021-08-28 15:36:13 +0100

    Map needs to return need components

--- src/components/NeedList/NeedList.js
+++ src/components/NeedList/NeedList.js
@@ -1,8 +1,16 @@
 import './NeedList.css';
+import Need from "../Need/Need";
 
-const NeedList = () => {
+const NeedList = ({ needs }) => {
+
+    const allNeeds = needs.map(need => {
+        return <Need key={need.id} id={need.id} pointOfContact={need.point_of_contact} title={need.title} description={need.description} startTime={need.start_time} endTime={need.end_time} zipCode={need.zip_code} supporters={need.supporters}/>
+    })
+
     return ( 
-        <p>Hello World</p>
+        <article>
+            {allNeeds}
+        </article>
      );
 }
  
