9529c65861828ab37d46fcbf09549e555634eeac
Shauna Myers <shaunamyers26@gmail.com>
2021-08-28 15:58:31 +0100

    Add JSX for Need card

--- src/components/Need/Need.js
+++ src/components/Need/Need.js
@@ -1,9 +1,18 @@
+import './Need.css'
 import { needsData } from "../../needsData";
 
-const Need = ({ id, pointOfContact, title, description, startTime, endTime, zipCode, supporters }) => {
+const Need = ({ pointOfContact, title, description, startTime, endTime, zipCode, supporters }) => {
     return ( 
         <div className="need-container">
-            <h4>Hello</h4>
+            <h4>{title}</h4>
+            <p>{description}</p>
+            <p>{zipCode}</p>
+            <div className="time-container">
+                <p>{startTime}-</p>
+                <p>{endTime}</p>
+            </div>
+            <p>Volunteers needed: {supporters}</p>
+            <p>Contact: {pointOfContact}</p>
         </div>
      );
 }
