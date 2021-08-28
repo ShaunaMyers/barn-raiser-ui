786d47a8b39a9a6ad323046cdf4a5cef519568c8
Shauna Myers <shaunamyers26@gmail.com>
2021-08-28 15:58:03 +0100

    Fix supporters not showin

--- src/components/NeedList/NeedList.js
+++ src/components/NeedList/NeedList.js
@@ -4,12 +4,14 @@ import Need from "../Need/Need";
 const NeedList = ({ needs }) => {
 
     const allNeeds = needs.map(need => {
-        return <Need key={need.id} id={need.id} pointOfContact={need.point_of_contact} title={need.title} description={need.description} startTime={need.start_time} endTime={need.end_time} zipCode={need.zip_code} supporters={need.supporters}/>
+        return <Need key={need.id} id={need.id} pointOfContact={need.point_of_contact} title={need.title} description={need.description} startTime={need.start_time} endTime={need.end_time} zipCode={need.zip_code} supporters={need.requested_supporters}/>
     })
 
     return ( 
         <article>
-            {allNeeds}
+            <div className="all-needs">
+                {allNeeds}
+            </div>
         </article>
      );
 }
