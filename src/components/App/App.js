64340282697fcafbe670cf4402ccb47e8c56ca30
Shauna Myers <shaunamyers26@gmail.com>
2021-08-28 15:24:49 +0100

    Add useEffect and useState

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,29 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 import NeedForm from '../NeedForm/NeedForm';
 import NeedList from '../NeedList/NeedList';
+import { needsData } from '../../needsData';
 
 function App() {
+
+  const [ needs, setNeeds ] = useState([])
+
+  useEffect(() => {
+    setNeeds(needsData.allActiveNeeds)
+  }, [])
+
   return (
     <main>
       <header>
         <h1>BarnRaiser</h1>
       </header>
-      <section className="help-questions">
-        <h2>Are You Looking for Assistance?</h2>
+      <section className="main-container">
+        {/* <h2>Are You Looking for Assistance?</h2>
         <button>I Need Help</button>
         <h3>Can You Offer Assistance?</h3>
-        <button>Give Help</button>
+        <button>Give Help</button> */}
         {/* <NeedForm /> */}
+        <NeedList needs={needs} />
       </section>
     </main>
   );
