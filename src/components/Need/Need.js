81f360b1b37e22650fbc0e89868fddad055d8fcd
Shauna Myers <shaunamyers26@gmail.com>
2021-08-28 15:36:31 +0100

    Build basic Need component with props

---
+++ src/components/Need/Need.js
@@ -0,0 +1,11 @@
+import { needsData } from "../../needsData";
+
+const Need = ({ id, pointOfContact, title, description, startTime, endTime, zipCode, supporters }) => {
+    return ( 
+        <div className="need-container">
+            <h4>Hello</h4>
+        </div>
+     );
+}
+ 
+export default Need;
