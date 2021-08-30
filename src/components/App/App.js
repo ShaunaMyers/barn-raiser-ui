1be482bbc43198a5c9ae1763257c02f6f1a46db5
Shauna Myers <shaunamyers26@gmail.com>
2021-08-30 23:51:56 +0100

    Add query and useQuery

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,63 +1,85 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import './App.css';
 import NeedForm from '../NeedForm/NeedForm';
 import NeedList from '../NeedList/NeedList';
 import { Route, Link } from 'react-router-dom';
-import { needsData } from '../../needsData';
-import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from '@apollo/client';
-
-const client = new ApolloClient({
-  uri: 'https://barn-raiser-be.herokuapp.com/graphql',
-  cache: new InMemoryCache()
-});
+// import { needsData } from '../../needsData';
+import { useQuery, gql } from '@apollo/client';
 
 function App() {
 
-  const [ needs, setNeeds ] = useState([])
+  // const [ needs, setNeeds ] = useState([])
 
-  useEffect(() => {
-    setNeeds(needsData.allActiveNeeds)
-  }, [])
+  // useEffect(() => {
+  //   setNeeds(needsData.allActiveNeeds)
+  // }, [])
 
-  const addNeed = (newNeed) => {
-    setNeeds([...needs, newNeed])
+  // const addNeed = (newNeed) => {
+  //   setNeeds([...needs, newNeed])
+  // }
+
+  const allActiveNeedsQuery = gql`{
+    allActiveNeeds{
+      id
+      title
+      description  
+      pointOfContact
+      startTime 
+      endTime 
+      zipCode 
+      supportersNeeded
+      status
+    }
+  }`;
+
+  const { loading, error, data } = useQuery(allActiveNeedsQuery);
+
+  if (loading) {
+    return(
+      <p>Loading...</p>
+    )
+  } else if (error) {
+    return(
+      <p>error</p>
+    )
+  } else {
+    console.log('data', data)
+    return (
+      // <ApolloProvider client={client}>
+        <main>
+          <header>
+            <Link to="/">
+              <h1>BarnRaiser</h1>
+            </Link>
+          </header>
+          <Route exact path="/" render={() => {
+            return (
+              <section className="main-container">
+                <h2>Are You Looking for Assistance?</h2>
+                <Link to="/NeedForm">
+                  <button>I Need Help</button>
+                </Link>
+                <h3>Can You Offer Assistance?</h3>
+                <Link to="/NeedList">
+                  <button>Give Help</button>
+                </Link>
+              </section>
+            )
+          }}/>
+          <Route exact path="/NeedForm" render={() => {
+            return (
+              <NeedForm/>
+              )
+            }}/>
+          <Route exact path="/NeedList" render={() => {
+            return (
+              <NeedList needs={data.allActiveNeeds} />
+              )
+            }}/>
+        </main>
+      // </ApolloProvider>
+    );
   }
-
-  return (
-    <ApolloProvider client={client}>
-      <main>
-        <header>
-          <Link to="/">
-            <h1>BarnRaiser</h1>
-          </Link>
-        </header>
-        <Route exact path="/" render={() => {
-          return (
-            <section className="main-container">
-              <h2>Are You Looking for Assistance?</h2>
-              <Link to="/NeedForm">
-                <button>I Need Help</button>
-              </Link>
-              <h3>Can You Offer Assistance?</h3>
-              <Link to="/NeedList">
-                <button>Give Help</button>
-              </Link>
-            </section>
-          )
-        }}/>
-        <Route exact path="/NeedForm" render={() => {
-          return (
-            <NeedForm addNeed={addNeed}/>
-            )
-          }}/>
-        <Route exact path="/NeedList" render={() => {
-          return (
-            <NeedList needs={needs} />
-            )
-          }}/>
-      </main>
-    </ApolloProvider>
-  );
 }
 
 export default App;
