8077ed92669f98b398e2b29b6affca92e8b6a1cc
Dean Cook <deancook371@gmail.com>
2021-08-30 11:32:21 -0700

    Flesh out basic tests checking correct elements are displayed

--- cypress/integration/help-request.js
+++ cypress/integration/help-request.js
@@ -4,13 +4,28 @@ describe('Barn Raiser Help Request Form', () => {
     cy.get('button').contains('I Need Help').click();
   });
 
+  it('should allow the user to go back to the home page by clicking on the BarnRaiser logo', () => {
+    cy.get('h1').contains('BarnRaiser').click();
+    cy.get('h2').contains('Are You Looking for Assistance?').should('be.visible');
+    cy.get('button').contains('I Need Help').should('be.visible');
+    cy.get('h3').contains('Can You Offer Assistance?').should('be.visible');
+    cy.get('button').contains('Give Help').should('be.visible');
+  });
+
   it('should show the correct form on the page', () => {
-
+    cy.get('form').should('be.visible');
+    cy.get('input[name="email"]').should('be.visible');
+    cy.get('input[name="zipCode"]').should('be.visible');
+    cy.get('input[name="needDate"]').should('be.visible');
+    cy.get('input[name="startTime"]').should('be.visible');
+    cy.get('input[name="endTime"]').should('be.visible');
+    cy.get('input[name="volunteersNeeded"]').should('be.visible');
+    cy.get('input[name="needTitle"]').should('be.visible');
+    cy.get('input[name="needDescription"]').should('be.visible');
+    cy.get('button').contains('Submit').should('be.visible');
   });
 
-  it('should allow the user to input their data and create a new help request', () => {
-
-  });
+  it('should allow the user to input their data and create a new help request');
 
   it('should give a visual indicator that the request has been submitted / recieved');
 
