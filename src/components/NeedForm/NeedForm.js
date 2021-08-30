6623465230fe7c75e1039d55343f15f12b661748
Shauna Myers <shaunamyers26@gmail.com>
2021-08-30 15:53:35 +0100

    Set time state onChange

--- src/components/NeedForm/NeedForm.js
+++ src/components/NeedForm/NeedForm.js
@@ -16,6 +16,10 @@ const NeedForm = () => {
         e.target.name === 'email' && setEmail(e.target.value);
         e.target.name === 'zipCode' && setZipCode(e.target.value);
         e.target.name === 'needDate' && setDate(e.target.value);
+        e.target.name === 'startTime' && setStartTime(e.target.value);
+        e.target.name === 'endTime' && setEndTime(e.target.value);
+
+
 
     }
 
@@ -24,8 +28,8 @@ const NeedForm = () => {
             <input onChange={handleInputChange} type="email" name="email" placeholder="Email Address" value={email}/>
             <input onChange={handleInputChange} type="text" name="zipCode" placeholder="Zip Code" value={zipCode}/>
             <input onChange={handleInputChange} type="date" name="needDate" min={new Date().toISOString().slice(0,10)} max="2025-08-27"/>
-            <input onChange={handleInputChange} type="time" name="start-time"/>
-            <input onChange={handleInputChange} type="time" name="end-time"/>
+            <input onChange={handleInputChange} type="time" name="startTime"/>
+            <input onChange={handleInputChange} type="time" name="endTime"/>
             <input onChange={handleInputChange} type="number" name="volunteers-needed" min="1" max="100"/>
             <input onChange={handleInputChange} type="text" name="need-description" placeholder="Describe your need"/>
             <button>Submit</button>
