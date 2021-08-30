c77b4df571dfcfa886d7d9bc395b65d8e279cbb8
Dean Cook <deancook371@gmail.com>
2021-08-30 14:34:09 -0700

    Update button colors & hover states

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -21,19 +21,19 @@ function App() {
     <main>
       <header>
         <Link to="/">
-          <h1>BarnRaiser</h1>
+          <h1 className="logo">BarnRaiser</h1>
         </Link>
       </header>
       <Route exact path="/" render={() => {
         return (
           <section className="main-container">
-            <img src="https://c.stocksy.com/a/tk2400/z9/963907.jpg" />
+            <img src="https://c.stocksy.com/a/tk2400/z9/963907.jpg" className="banner-img" alt="Two hands reaching out towards one another" />
             <div className="intro-text">
-              <h2>Are You Looking for Assistance?</h2>
+              <h2 className="header-text">Are You Looking for Assistance?</h2>
               <Link to="/NeedForm">
                 <button>I Need Help</button>
               </Link>
-              <h3>Can You Offer Assistance?</h3>
+              <h3 className="header-text">Can You Offer Assistance?</h3>
               <Link to="/NeedList">
                 <button>Give Help</button>
               </Link>
