afa4bb25fe66e2257cbd6f3c8464be8dd099f66b
Shauna Myers <shaunamyers26@gmail.com>
2021-08-30 17:00:33 +0100

    Change snake to camelCase

--- src/needsData.js
+++ src/needsData.js
@@ -2,30 +2,30 @@ export const needsData = { "allActiveNeeds":
   [
     { 
       "id": 156432,
-      "point_of_contact": "MargeInTheFlesh@comcast.net",
+      "pointOfContact": "MargeInTheFlesh@comcast.net",
       "title": "Yard Work",
       "description": "I would like to have my yard raked and my gutters cleaned",
-      "start_time": "2021-09-27 12:00:00.000000 -0600",
-      "end_time": "2021-08-27 16:00:00.000000 -0600",
-      "zip_code": 80214,
-      "requested_supporters": 2 },
+      "startTime": "2021-09-27 12:00:00.000000 -0600",
+      "endTime": "2021-08-27 16:00:00.000000 -0600",
+      "zipCode": 80214,
+      "supportersNeeded": 2 },
      { 
       "id": 462346,
-      "point_of_contact": "GroverCleveland1234@turing.edu",
+      "pointOfContact": "GroverCleveland1234@turing.edu",
       "title": "Event Volunteers",
       "description": "Organizing a charity event to raise money for a family whose house burned down. Need volunteers to set up tables, serve food, run game booths.",
-      "start_time": "2021-09-15 15:30:00.000000 -0600",
-      "end_time": "2021-09-15 19:00:00.000000 -0600",
-      "zip_code": 80995,
-      "requested_supporters": 5 },
+      "startTime": "2021-09-15 15:30:00.000000 -0600",
+      "endTime": "2021-09-15 19:00:00.000000 -0600",
+      "zipCode": 80995,
+      "supportersNeeded": 5 },
      { 
       "id": 977322,
-      "point_of_contact": "GeorgeElliot4Evuh@gmail.com",
+      "pointOfContact": "GeorgeElliot4Evuh@gmail.com",
       "title": "Please Read To Me",
       "description": "Will someone come read to me? I have lost my eyesight. I prefer the classics.",
-      "start_time": "2021-10-04 10:00:00.000000 -0600",
-      "end_time": "2021-10-04 10:00:00.000000 -0600",
-      "zip_code": 80014,
-      "requested_supporters": 1 }
+      "startTime": "2021-10-04 10:00:00.000000 -0600",
+      "endTime": "2021-10-04 10:00:00.000000 -0600",
+      "zipCode": 80014,
+      "supportersNeeded": 1 }
   ]
 };
