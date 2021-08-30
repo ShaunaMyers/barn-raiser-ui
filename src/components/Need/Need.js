2359aa630ecc0922f470f8e68d16b5a09cdc470f
Dean Cook <deancook371@gmail.com>
2021-08-30 15:30:31 -0700

    Update need card styling

--- src/components/Need/Need.js
+++ src/components/Need/Need.js
@@ -2,19 +2,23 @@ import './Need.css'
 import { needsData } from "../../needsData";
 
 const Need = ({ pointOfContact, title, description, startTime, endTime, zipCode, supporters }) => {
-    return ( 
-        <div className="need-container">
-            <h4>{title}</h4>
-            <p>{description}</p>
-            <p>{zipCode}</p>
-            <div className="time-container">
-                <p>{startTime}-</p>
-                <p>{endTime}</p>
-            </div>
-            <p>Volunteers needed: {supporters}</p>
-            <p>Contact: {pointOfContact}</p>
-        </div>
-     );
+  return (
+    <div className="need-container">
+      <div className="need-description-info">
+        <h4>{title}</h4>
+        <p>{description}</p>
+        <p>{zipCode}</p>
+      </div>
+      <div className="time-container">
+        <p>{startTime}-</p>
+        <p>{endTime}</p>
+      </div>
+      <div className="need-volunteer-info">
+        <p>Volunteers needed: {supporters}</p>
+        <p>Contact: {pointOfContact}</p>
+      </div>
+    </div>
+   );
 }
- 
+
 export default Need;
