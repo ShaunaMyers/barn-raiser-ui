5b9b06003bd7200d5cc85b0ba4389fce341d9d77
Shauna Myers <shaunamyers26@gmail.com>
2021-08-30 15:52:26 +0100

    Set date state onChange

--- src/components/NeedForm/NeedForm.js
+++ src/components/NeedForm/NeedForm.js
@@ -15,13 +15,15 @@ const NeedForm = () => {
     const handleInputChange = (e) => {
         e.target.name === 'email' && setEmail(e.target.value);
         e.target.name === 'zipCode' && setZipCode(e.target.value);
+        e.target.name === 'needDate' && setDate(e.target.value);
+
     }
 
     return ( 
         <form>
             <input onChange={handleInputChange} type="email" name="email" placeholder="Email Address" value={email}/>
             <input onChange={handleInputChange} type="text" name="zipCode" placeholder="Zip Code" value={zipCode}/>
-            <input onChange={handleInputChange} type="date" name="need-date" min={new Date().toISOString().slice(0,10)} max="2025-08-27"/>
+            <input onChange={handleInputChange} type="date" name="needDate" min={new Date().toISOString().slice(0,10)} max="2025-08-27"/>
             <input onChange={handleInputChange} type="time" name="start-time"/>
             <input onChange={handleInputChange} type="time" name="end-time"/>
             <input onChange={handleInputChange} type="number" name="volunteers-needed" min="1" max="100"/>
