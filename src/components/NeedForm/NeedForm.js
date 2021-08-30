2fd8d6205830af808ac5348fbebe5a5435dc1f10
Shauna Myers <shaunamyers26@gmail.com>
2021-08-30 16:20:52 +0100

    Destructure target name & value

--- src/components/NeedForm/NeedForm.js
+++ src/components/NeedForm/NeedForm.js
@@ -13,29 +13,25 @@ const NeedForm = () => {
 
 
     const handleInputChange = (e) => {
-        e.target.name === 'email' && setEmail(e.target.value);
-        e.target.name === 'zipCode' && setZipCode(e.target.value);
-        e.target.name === 'needDate' && setDate(e.target.value);
-        e.target.name === 'startTime' && setStartTime(e.target.value);
-        e.target.name === 'endTime' && setEndTime(e.target.value);
-        e.target.name === 'volunteersNeeded' && setVolunteers(e.target.value);
-        e.target.name === 'description' && setDescription(e.target.value);
-
-
-
-
-
+        const { name, value } = e.target;
+        name === 'email' && setEmail(value);
+        name === 'zipCode' && setZipCode(value);
+        name === 'needDate' && setDate(value);
+        name === 'startTime' && setStartTime(value);
+        name === 'endTime' && setEndTime(value);
+        name === 'volunteersNeeded' && setVolunteers(value);
+        name === 'needDescription' && setDescription(value);
     }
 
     return ( 
         <form>
             <input onChange={handleInputChange} type="email" name="email" placeholder="Email Address" value={email}/>
-            <input onChange={handleInputChange} type="text" name="zipCode" placeholder="Zip Code" value={zipCode}/>
+            <input onChange={handleInputChange} type="number" name="zipCode" placeholder="Zip Code" value={zipCode}/>
             <input onChange={handleInputChange} type="date" name="needDate" min={new Date().toISOString().slice(0,10)} max="2025-08-27"/>
             <input onChange={handleInputChange} type="time" name="startTime"/>
             <input onChange={handleInputChange} type="time" name="endTime"/>
             <input onChange={handleInputChange} type="number" name="volunteersNeeded" min="1" max="100" value={volunteers}/>
-            <input onChange={handleInputChange} type="text" name="need-description" placeholder="Describe your need" value={description}/>
+            <input onChange={handleInputChange} type="text" name="needDescription" placeholder="Describe your need" value={description}/>
             <button>Submit</button>
         </form>
      );
