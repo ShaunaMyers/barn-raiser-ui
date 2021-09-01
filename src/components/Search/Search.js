ee67d52c52f4f6f0a88e6e874edc9c31b1c8eec1
Shauna Myers <shaunamyers26@gmail.com>
2021-09-01 22:19:17 +0100

    Add & style inputs

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -7,6 +7,12 @@ const Search = () => {
             return (
             <form>
                 <input type="text" name="search" placeholder="Search for need entries"/>
+                    <div className="checkbox-container">
+                            <input className="zip-code-box" type="checkbox" id="zipCodeBox"/>
+                            <label for="zipCodeBox">Zip Code</label>
+                            <input className="checkboxes" type="checkbox" id="dateBox"/>
+                            <label for="dateBox">Date</label>
+                    </div>
                 <button className="search-button">Search</button>
             </form>
             )
