5e5d1ab96c3076fb23ae4ebbfdbb71516f2ea415
Shauna Myers <shaunamyers26@gmail.com>
2021-09-01 15:31:06 +0100

    Write basic func to formatDate

--- src/components/NeedList/NeedList.js
+++ src/components/NeedList/NeedList.js
@@ -3,8 +3,12 @@ import Need from "../Need/Need";
 
 const NeedList = ({ needs }) => {
 
+    const formatDate = (date) => {
+        
+    }
+
     const allNeeds = needs.map(need => {
-        return <Need key={need.id} id={need.id} pointOfContact={need.pointOfContact} title={need.title} description={need.description} startTime={need.startTime} endTime={need.endTime} zipCode={need.zipCode} supporters={need.supportersNeeded}/>
+        return <Need key={need.id} id={need.id} pointOfContact={need.pointOfContact} date={formatDate(need.startTime)} title={need.title} description={need.description} startTime={need.startTime} endTime={need.endTime} zipCode={need.zipCode} supporters={need.supportersNeeded}/>
     })
 
     return (
