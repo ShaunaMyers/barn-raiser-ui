360fa7436b0b646e6022f9f936f956c1a0858e25
Shauna Myers <shaunamyers26@gmail.com>
2021-09-01 22:44:55 +0100

    Change state and function names

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -4,39 +4,41 @@ import './Search.css';
 
 const Search = () => {
 
-    const [isChecked1, setIsChecked1] = useState(false);
-    const [isChecked2, setIsChecked2] = useState(false);
+    const [zipCodeBox, setZipCodeBox] = useState(false);
+    const [dateBox, setDateBox] = useState(false);
     const [searchInput, setSearchInput] = useState('');
 
-    const handleSearchSubmit = () => {
-
+    const onSearchSubmit = () => {
+        // Check if checked1 or 2 have value
+        // AND check if searchInput has value
+        // If so, filter through data by that value and return the needs that match
     }
 
     const changeCheckedBoxes = (num) => {
         if (num === 1) {
-            setIsChecked1(true);
-            setIsChecked2(false);
+            setZipCodeBox(true);
+            setDateBox(false);
         } else {
-            setIsChecked2(true);
-            setIsChecked1(false);
+            setDateBox(true);
+            setZipCodeBox(false);
         }
     }
 
     return ( 
-        <Route exact path="/NeedList" render={() => {
-            return (
+        // <Route exact path="/NeedList" render={() => {
+        //     return (
             <form>
                 <input onChange={(e) => setSearchInput(e.target.value)} type="text" name="search" placeholder="Search for need entries" value={searchInput}/>
                     <div className="checkbox-container">
-                            <input onChange={() => changeCheckedBoxes(1)} className="zip-code-box" type="checkbox" id="zipCodeBox" checked={isChecked1}/>
+                            <input onChange={() => changeCheckedBoxes(1)} className="zip-code-box" type="checkbox" id="zipCodeBox" checked={zipCodeBox}/>
                             <label for="zipCodeBox">Zip Code</label>
-                            <input onChange={() => changeCheckedBoxes(2)} className="checkboxes" type="checkbox" id="dateBox" checked={isChecked2}/>
+                            <input onChange={() => changeCheckedBoxes(2)} className="checkboxes" type="checkbox" id="dateBox" checked={dateBox}/>
                             <label for="dateBox">Date</label>
                     </div>
-                <button onClick={handleSearchSubmit} className="search-button">Search</button>
+                <button onClick={onSearchSubmit} className="search-button">Search</button>
             </form>
-            )
-        }}/>
+        //     )
+        // }}/>
      );
 }
  
