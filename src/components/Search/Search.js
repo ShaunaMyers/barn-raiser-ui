9ea5bb14731d6af278bcd38b4ab009c473d310ae
Shauna Myers <shaunamyers26@gmail.com>
2021-09-01 22:53:29 +0100

    Create basic onSearchSubmit conditionals

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -7,11 +7,21 @@ const Search = () => {
     const [zipCodeBox, setZipCodeBox] = useState(false);
     const [dateBox, setDateBox] = useState(false);
     const [searchInput, setSearchInput] = useState('');
+    const [isError, setIsError] = useState(false);
 
     const onSearchSubmit = () => {
+        e.preventDefault();
         // Check if checked1 or 2 have value
         // AND check if searchInput has value
-        // If so, filter through data by that value and return the needs that match
+        // If so, pass checkBox and searchData up to App
+        // If not use Error component to display a message 
+        if (searchInput && zipCodeBox) {
+            handleSearchSubmit(searchInput, 'zipCode');
+        } else if (searchInput && dateBox) {
+            handleSearchSubmit(searchInput, 'date');
+        } else {
+            setIsError(true)
+        }
     }
 
     const changeCheckedBoxes = (num) => {
