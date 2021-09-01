5c1c587350013d0ce680ec9eb137fab6d0d84a38
Shauna Myers <shaunamyers26@gmail.com>
2021-09-01 20:26:31 +0100

    Add route to search component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,9 +1,16 @@
+import './Search.css';
+import { Route } from 'react-router-dom';
+
 const Search = () => {
     return ( 
-        <form>
-            <input type="text" name="search" placeholder="Search for need entries"/>
-            <button className="search-button">Search</button>
-        </form>
+        <Route exact path="/NeedList" render={() => {
+            return (
+            <form>
+                <input type="text" name="search" placeholder="Search for need entries"/>
+                <button className="search-button">Search</button>
+            </form>
+            )
+        }}/>
      );
 }
  
