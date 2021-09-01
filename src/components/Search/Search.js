aaaf045fa7b10f807a2bc14f368fcf5e1d95b935
Shauna Myers <shaunamyers26@gmail.com>
2021-09-01 22:22:00 +0100

    Establish isChecked in state

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,16 +1,20 @@
-import './Search.css';
+import { useState } from 'react';
 import { Route } from 'react-router-dom';
+import './Search.css';
 
 const Search = () => {
+
+    const [isChecked, setIsChecked] = useState(false);
+
     return ( 
         <Route exact path="/NeedList" render={() => {
             return (
             <form>
                 <input type="text" name="search" placeholder="Search for need entries"/>
                     <div className="checkbox-container">
-                            <input className="zip-code-box" type="checkbox" id="zipCodeBox"/>
+                            <input className="zip-code-box" type="checkbox" id="zipCodeBox" checked={isChecked}/>
                             <label for="zipCodeBox">Zip Code</label>
-                            <input className="checkboxes" type="checkbox" id="dateBox"/>
+                            <input className="checkboxes" type="checkbox" id="dateBox" checked={isChecked}/>
                             <label for="dateBox">Date</label>
                     </div>
                 <button className="search-button">Search</button>
