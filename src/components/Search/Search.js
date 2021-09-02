fd29950d2c7a11f84311b856c0712ca6a565c74e
Shauna Myers <shaunamyers26@gmail.com>
2021-09-02 15:32:08 +0100

    Add functionality to view all needs w/ link click

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,20 +3,24 @@ import { useState } from 'react';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import './Search.css';
 
-const Search = ({ handleSearchSubmit }) => {
+const Search = ({ handleSearchSubmit, handleViewAllNeeds }) => {
 
     const [zipCodeBox, setZipCodeBox] = useState(false);
     const [dateBox, setDateBox] = useState(false);
     const [searchInput, setSearchInput] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
+    const [successfulSearch, setSuccessfulSearch] = useState(false);
 
     const onSearchSubmit = (e) => {
         e.preventDefault();
         setErrorMessage('');
+        setSuccessfulSearch(false);
         if (searchInput && zipCodeBox) {
             handleSearchSubmit(searchInput, 'zipCode');
+            setSuccessfulSearch(true);
         } else if (searchInput && dateBox) {
             handleSearchSubmit(searchInput, 'date');
+            setSuccessfulSearch(true);
         } else if (!zipCodeBox && !dateBox){
             setErrorMessage('Please check a box to complete your search')
         } else {
@@ -41,11 +45,15 @@ const Search = ({ handleSearchSubmit }) => {
                 <input onChange={(e) => setSearchInput(e.target.value)} type="text" name="search" placeholder="Search for need entries" value={searchInput}/>
                     <div className="checkbox-container">
                             <input onChange={() => changeCheckedBoxes(1)} className="zip-code-box" type="checkbox" id="zipCodeBox" checked={zipCodeBox}/>
-                            <label for="zipCodeBox">Zip Code</label>
+                            <label>Zip Code</label>
                             <input onChange={() => changeCheckedBoxes(2)} className="checkboxes" type="checkbox" id="dateBox" checked={dateBox}/>
-                            <label for="dateBox">Date</label>
+                            <label>Date</label>
                     </div>
                 <button onClick={onSearchSubmit} className="search-button">Search</button>
+                {successfulSearch && 
+                // <button onClick={handleViewAllNeeds} className="see-all-button">See All Needs</button>
+                <a onClick={handleViewAllNeeds} className="see-all-link">See All Needs</a>
+                }
                 {errorMessage &&
                     <ErrorMessage errorMessage={errorMessage}/>
                 }
