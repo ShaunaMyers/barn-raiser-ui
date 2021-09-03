18ab832e3b31e0cc9f0e19e6a19782257b9f9ced
Shauna Myers <shaunamyers26@gmail.com>
2021-09-03 01:19:46 +0100

    Fix single digit day display

--- src/components/NeedList/NeedList.js
+++ src/components/NeedList/NeedList.js
@@ -7,12 +7,13 @@ const NeedList = ({ needs }) => {
         const formattedDate = new Date(date);
         const [month, day, year] = [(1 + formattedDate.getMonth()), formattedDate.getDate(), formattedDate.getFullYear()];
         const monthFormat = month > 9 ? month : '0' + month;
-        return `${monthFormat}/${day}/${year}`;
+        return `${monthFormat}/${(day < 10) ? '0' + day : day}/${year}`;
     }
 
     const formatTime = (time) => {
         const shortenedTime = time.slice(11, 16);
         const [hours, minutes] = shortenedTime.split(':');
+        // single digits on minutes
         return `${(hours > 12) ? hours - 12 : hours}:${minutes}${(hours >= 12) ? 'pm' : 'am'}`;
     }
 
