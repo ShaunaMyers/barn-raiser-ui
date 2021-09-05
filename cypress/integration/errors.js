add429e9a287c4f6a5e23b662d068298ef7b9e07
Dean Cook <deancook371@gmail.com>
2021-09-05 15:43:58 -0600

    Write test for URL redirecting

--- cypress/integration/errors.js
+++ cypress/integration/errors.js
@@ -1,5 +1,13 @@
 describe('Barn Raiser Error Handling', () => {
-  it('should display an error page to the user and ask them to re-route back home if they go to a non-existent page');
+  it('should re-route the user back home if they go to a non-existent page', () => {
+    cy.visit('http://localhost:3000/garbage_url_test');
+    cy.url().should('eq', 'http://localhost:3000/');
+    cy.get('h1').contains('BarnRaiser').should('be.visible');
+    cy.get('h2').contains('Are You Looking for Assistance?').should('be.visible');
+    cy.get('button').contains('I Need Help').should('be.visible');
+    cy.get('h3').contains('Can You Offer Assistance?').should('be.visible');
+    cy.get('button').contains('Give Help').should('be.visible');
+  });
 
   it('should notify the user if their help request cannot go through to the server');
 
