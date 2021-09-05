6eddbc271b0c77ffd3d28bbe05e01f6e8f8cc2c7
Shauna Myers <shaunamyers26@gmail.com>
2021-09-05 20:32:56 +0100

    Add categories and separate with bullet

--- src/components/Need/Need.js
+++ src/components/Need/Need.js
@@ -1,12 +1,25 @@
 import './Need.css'
 
-const Need = ({ pointOfContact, title, description, date, startTime, endTime, zipCode, supporters }) => {
+const Need = ({ pointOfContact, title, categories, description, date, startTime, endTime, zipCode, supporters }) => {
+
+  const formatCategoriesText = (tag, index) => {
+    if (index !== 0 && index !== categories.length - 1) return `  •  ${tag}  •  ` 
+    else return tag
+  }
+
+  const allCategories = categories.map((category, index) => {
+    return <p key={category.id}>{formatCategoriesText(category.tag, index)}</p>
+  })
+
+
   return (
     <div className="need-container">
       <div className="need-info">
         <h4>{title}</h4>
         <p className="bold">Details:</p>
         <p>{description}</p>
+        <p className="bold">Categories:</p>
+        <div className="category-container">{allCategories}</div>
         <p className="bold">{zipCode}</p>
         <p>{date}</p>
       </div>
