d5f442c6df1445ce84159d3305c655863b4c02c3
Dean Cook <deancook371@gmail.com>
2021-09-07 08:16:18 -0600

    Define component structure of need detail page

---
+++ src/components/NeedDetailPage/NeedDetailPage.js
@@ -0,0 +1,7 @@
+import './NeedDetailPage.css'
+
+const NeedDetailPage = ({ pointOfContact, title, categories, description, date, startTime, endTime, zipCode, supporters }) => {
+  
+}
+
+export default NeedDetailPage;
