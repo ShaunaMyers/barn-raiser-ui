636038806bb11af5b5406a4a4eb0131796f964c5
Shauna Myers <shaunamyers26@gmail.com>
2021-09-07 22:09:35 +0100

    Add time grouping styling

--- src/components/NeedForm/NeedForm.js
+++ src/components/NeedForm/NeedForm.js
@@ -143,19 +143,22 @@ const NeedForm = () => {
     return (
       <form>
         {!!isSubmitted && <h3 className="success-message">Success! Your submission has been recorded. <Link to="/NeedList">Take me there.</Link></h3>}
-        <div className="contact-container">
-          <p className="contact-title">1. Contact & Location Information</p>
+        <div className="contact-grouping">
+          <p className="grouping-title">1. Contact & Location Information</p>
           <label for="email">Email:</label>
           <input onChange={handleInputChange} type="email" name="email" id="email" placeholder="Email Address" value={pointOfContact}/>
           <label for="zipCode">Zip Code:</label>
           <input onChange={handleInputChange} type="text" name="zipCode" id="zipCode" placeholder="Zip Code" value={zipCode}/>
         </div>
-        <label for="needDate">Date:</label>
-        <input onChange={handleInputChange} type="date" name="needDate" id="needDate" min={new Date().toISOString().slice(0,10)} max="2025-08-27"/>
-        <label for="startTime">Start Time:</label>
-        <input onChange={handleInputChange} type="time" name="startTime" id="startTime"/>
-        <label for="endTime">End Time:</label>
-        <input onChange={handleInputChange} type="time" name="endTime" id="endTime"/>
+        <div className="time-grouping">
+          <p className="grouping-title">2. Date & Time</p>
+          <label for="needDate">Date:</label>
+          <input onChange={handleInputChange} type="date" name="needDate" id="needDate" min={new Date().toISOString().slice(0,10)} max="2025-08-27"/>
+          <label for="startTime">Start Time:</label>
+          <input onChange={handleInputChange} type="time" name="startTime" id="startTime"/>
+          <label for="endTime">End Time:</label>
+          <input onChange={handleInputChange} type="time" name="endTime" id="endTime"/>
+        </div>
         <label for="volunteersNeeded">Number of Volunteers Needed:</label>
         <input onChange={handleInputChange} type="number" name="volunteersNeeded" id="volunteersNeeded" min="1" max="100" value={supportersNeeded}/>
         <label for="needTitle">Title:</label>
