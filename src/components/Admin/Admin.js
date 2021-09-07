ce421e505ca2238ef211a218730686d9a86eb10e
Shauna Myers <shaunamyers26@gmail.com>
2021-09-07 17:54:39 +0100

    Add classNames

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,21 +1,35 @@
+import './Admin.css';
+
 const Admin = () => {
     return ( 
         <section>
             <p>View Supporters By Category:</p>
-            <form>
-                <input type="checkbox" id="organizingCheck"/>
-                <label htmlFor="organizingCheck">Organizing / Event Management</label>
-                <input type="checkbox" id="handiworkCheck"/>
-                <label htmlFor="handiworkCheck">Handiwork</label>
-                <input type="checkbox" id="deliveryCheck"/>
-                <label htmlFor="deliveryCheck">Delivery</label>
-                <input type="checkbox" id="transportationCheck"/>
-                <label htmlFor="transportationCheck">Transportation</label>
-                <input type="checkbox" id="foodPrepCheck"/>
-                <label htmlFor="foodPrepCheck">Food Prep</label>
-                <input type="checkbox" id="otherCheck"/>
-                <label htmlFor="otherCheck">Other</label>
-                <button>View</button>
+            <form className="Admin-category-form">
+                <div className="inputs-labels">
+                    <input className="checkboxes" type="checkbox" id="organizingCheck"/>
+                    <label htmlFor="organizingCheck">Organizing / Event Management</label>
+                </div>
+                <div className="inputs-labels">
+                    <input className="checkboxes" type="checkbox" id="handiworkCheck"/>
+                    <label htmlFor="handiworkCheck">Handiwork</label>
+                </div>
+                <div className="inputs-labels">
+                    <input className="checkboxes" type="checkbox" id="deliveryCheck"/>
+                    <label htmlFor="deliveryCheck">Delivery</label>
+                </div>
+                <div className="inputs-labels">
+                    <input className="checkboxes" type="checkbox" id="transportationCheck"/>
+                    <label htmlFor="transportationCheck">Transportation</label>
+                </div>
+                <div className="inputs-labels">
+                    <input className="checkboxes" type="checkbox" id="foodPrepCheck"/>
+                    <label htmlFor="foodPrepCheck">Food Prep</label>
+                </div>
+                <div className="inputs-labels">
+                    <input className="checkboxes" type="checkbox" id="otherCheck"/>
+                    <label htmlFor="otherCheck">Other</label>
+                </div>
+                <button className="view-category-button">View</button>
             </form>
         </section>
      );
