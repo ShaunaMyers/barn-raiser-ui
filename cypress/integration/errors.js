1cc8f19e7815652ce56b1fd7de7dc9767acf87c6
Dean Cook <deancook371@gmail.com>
2021-09-07 08:54:51 -0600

    Remove broken redirect route & associated test

--- cypress/integration/errors.js
+++ cypress/integration/errors.js
@@ -1,11 +1,3 @@
 describe('Barn Raiser Error Handling', () => {
-  it('should re-route the user back home if they go to a non-existent page', () => {
-    cy.visit('http://localhost:3000/garbage_url_test');
-    cy.url().should('eq', 'http://localhost:3000/');
-    cy.get('h1').contains('BarnRaiser').should('be.visible');
-    cy.get('h2').contains('Are You Looking for Assistance?').should('be.visible');
-    cy.get('button').contains('I Need Help').should('be.visible');
-    cy.get('h3').contains('Can You Offer Assistance?').should('be.visible');
-    cy.get('button').contains('Give Help').should('be.visible');
-  });
+  
 });
