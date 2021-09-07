0cdc8689ac1876642c02d902bd4e7bc0a56452af
Shauna Myers <shaunamyers26@gmail.com>
2021-09-07 19:19:32 +0100

    Write function to check & uncheck boxes

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -2,16 +2,16 @@ import { useState } from 'react';
 import { useQuery, gql } from '@apollo/client';
 import './Admin.css';
 
-const CATEGORIES_QUERY = gql`{
-    allCategories{
-        id
-        tag
-        supporters {
-            name
-            email
-        }
-    }
-  }`;
+// const CATEGORIES_QUERY = gql`{
+//     allCategories{
+//         id
+//         tag
+//         supporters {
+//             name
+//             email
+//         }
+//     }
+//   }`;
 
 
 const Admin = () => {
@@ -23,37 +23,46 @@ const Admin = () => {
     const [foodPrepChecked, setFoodPrepChecked] = useState(false);
     const [otherChecked, setOtherChecked] = useState(false);
 
-    const { loading, error, data } = useQuery(CATEGORIES_QUERY);
+    // const { loading, error, data } = useQuery(CATEGORIES_QUERY);
 
-    console.log(data, 'data in ADMIN');
-    console.log(error, 'error');
+    // console.log(data, 'data in ADMIN');
+    // console.log(error, 'error');
+
+    const handleCheckBoxes = (num) => {
+        const checkboxes = ["OrganizingChecked", "HandiworkChecked", "DeliveryChecked", "TransportationChecked", "FoodPrepChecked", "OtherChecked"]
+        checkboxes.forEach((checkbox, index) => {
+            console.log(checkbox, 'checkbox')
+            num === index ? eval(`set${checkbox}(true)`) :
+            eval(`set${checkbox}(false)`);
+        })
+    }
 
     return ( 
         <section className="admin-section">
             <p className="admin-title">View Volunteers by Category:</p>
             <form className="admin-category-form">
                 <div className="inputs-labels">
-                    <input className="checkboxes" type="checkbox" id="organizingCheck" checked={organizingChecked}/>
+                    <input onChange={() => handleCheckBoxes(0)} className="checkboxes" type="checkbox" id="organizingCheck" checked={organizingChecked}/>
                     <label htmlFor="organizingCheck">Organizing/Event Management</label>
                 </div>
                 <div className="inputs-labels">
-                    <input className="checkboxes" type="checkbox" id="handiworkCheck" checked={handiworkChecked}/>
+                    <input onChange={() => handleCheckBoxes(1)} className="checkboxes" type="checkbox" id="handiworkCheck" checked={handiworkChecked}/>
                     <label htmlFor="handiworkCheck">Handiwork</label>
                 </div>
                 <div className="inputs-labels">
-                    <input className="checkboxes" type="checkbox" id="deliveryCheck" checked={deliveryChecked}/>
+                    <input onChange={() => handleCheckBoxes(2)} className="checkboxes" type="checkbox" id="deliveryCheck" checked={deliveryChecked}/>
                     <label htmlFor="deliveryCheck">Delivery</label>
                 </div>
                 <div className="inputs-labels">
-                    <input className="checkboxes" type="checkbox" id="transportationCheck" checked={transportationChecked}/>
+                    <input onChange={() => handleCheckBoxes(3)} className="checkboxes" type="checkbox" id="transportationCheck" checked={transportationChecked}/>
                     <label htmlFor="transportationCheck">Transportation</label>
                 </div>
                 <div className="inputs-labels">
-                    <input className="checkboxes" type="checkbox" id="foodPrepCheck" checked={foodPrepChecked}/>
+                    <input onChange={() => handleCheckBoxes(4)} className="checkboxes" type="checkbox" id="foodPrepCheck" checked={foodPrepChecked}/>
                     <label htmlFor="foodPrepCheck">Food Prep</label>
                 </div>
                 <div className="inputs-labels">
-                    <input className="checkboxes" type="checkbox" id="otherCheck" checked={otherChecked}/>
+                    <input onChange={() => handleCheckBoxes(5)} className="checkboxes" type="checkbox" id="otherCheck" checked={otherChecked}/>
                     <label htmlFor="otherCheck">Other</label>
                 </div>
             </form>
