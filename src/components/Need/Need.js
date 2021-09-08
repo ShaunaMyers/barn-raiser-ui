874fae0d1a408f80a612aebd54402c6d2c550f94
Shauna Myers <shaunamyers26@gmail.com>
2021-09-08 19:41:17 +0100

    Remove excess props being passed

--- src/components/Need/Need.js
+++ src/components/Need/Need.js
@@ -1,18 +1,7 @@
 import './Need.css'
 import { NavLink } from 'react-router-dom'
 
-const Need = ({ title, categories, date, zipCode, id }) => {
-
-  const formatCategoriesText = (tag, index) => {
-    if (categories.length === 2 && index === 0) return `${tag}  •  `
-    else if (index !== 0 && index !== categories.length - 1) return `  •  ${tag}  •  `
-    else return tag
-  }
-
-  const allCategories = categories.map((category, index) => {
-    return <p key={category.id}>{formatCategoriesText(category.tag, index)}</p>
-  })
-
+const Need = ({ title, date, zipCode, id }) => {
 
   return (
     <div className="need-container">
--- src/components/NeedList/NeedList.js
+++ src/components/NeedList/NeedList.js
@@ -1,11 +1,11 @@
 import './NeedList.css';
 import Need from "../Need/Need";
-import { formatDate, formatTime } from '../../utils';
+import { formatDate } from '../../utils';
 
 const NeedList = ({ needs }) => {
 
   const allNeeds = needs.map(need => {
-    return <Need key={need.id} id={need.id} pointOfContact={need.pointOfContact}  title={need.title} categories={need.categories} description={need.description} date={formatDate(need.startTime)} startTime={formatTime(need.startTime)} endTime={formatTime(need.endTime)} zipCode={need.zipCode} supporters={need.supportersNeeded}/>
+    return <Need key={need.id} id={need.id}  title={need.title} date={formatDate(need.startTime)} zipCode={need.zipCode}/>
   })
 
   return (
