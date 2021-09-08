fda2d3f0b52efa7638bd6dcb8467e9873857bbd3
Shauna Myers <shaunamyers26@gmail.com>
2021-09-08 19:15:02 +0100

    Style buttons for mobile responsiveness

--- src/components/NeedDetailPage/NeedDetailPage.js
+++ src/components/NeedDetailPage/NeedDetailPage.js
@@ -157,12 +157,13 @@ const NeedDetailPage = ({need_id}) => {
               <p>{formatDate(need.startTime)}</p>
               <p>{formatTime(need.startTime)} - {formatTime(need.endTime)}</p>
             </div>
-            <h3>{need.zipCode}</h3>
+            <h3>Zip Code:</h3>
+            <p>{need.zipCode}</p>
             <h3>{formatCategories(need.categories)}</h3>
             <h3>Volunteers: {need.supporters.length} / {need.supportersNeeded}</h3>
             <p>{need.description}</p>
           <div className="volunteer-action-container">
-            <a href={`mailto:${need.pointOfContact}?subject=RE:${need.title}`} className="contact-button"><button>Contact Requester</button></a>
+            <a href={`mailto:${need.pointOfContact}?subject=RE:${need.title}`}><button className="contact-button">Contact Requester</button></a>
             <button className="contact-volunteer-button" onClick={onClick}>Sign Up to Volunteer</button>
           </div>
           </div>
